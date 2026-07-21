2cc94a1e895a9e31999d2f9f60ab95ff242c9ee4
studioandriolo-oss <studioandriolo@gmail.com>
2026-07-21 10:10:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -389,11 +389,11 @@ if inviato:
 
 st.markdown("---")
     
-    # ==========================================
-    # PIANO COMMERCIALE E SCONTISTICA
-    # ==========================================
-    st.header("💰 Piano Commerciale")
-    st.markdown("Premiamo i volumi di lavoro continui.")
+# ==========================================
+# PIANO COMMERCIALE E SCONTISTICA
+# ==========================================
+st.header("💰 Piano Commerciale")
+st.markdown("Premiamo i volumi di lavoro continui")
     
     with st.info("📉 **Scontistica applicata per scaglioni di pratiche inviate:**"):
         st.markdown("""
